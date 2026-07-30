abcd021091b2f2a42e0a642a76aa94473eb14910
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2026-07-30 14:13:11 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rosa_ionic_1',
+  webDir: 'dist'
+};
+
+export default config;
